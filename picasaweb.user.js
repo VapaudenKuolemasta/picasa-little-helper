5710deedfdf2318938aaaa3fa15e5a391025e9ba
Vapauden Kuolemasta <kuolemasta@mail.ru>
2017-04-03 00:17:43 +0300

    Adaptation for goolge archives.

--- picasaweb.user.js
+++ picasaweb.user.js
@@ -1,23 +1,55 @@
 // ==UserScript==
 // @id              picasaweb
-// @name            Picasa little helper
-// @version         2.0
-// @description     Album download helper
-// @include         https://picasaweb.google.com/*
-// @match           https://picasaweb.google.com/*
+// @name            Picasa rss links generator
+// @version         3.0
+// @description     Generate link for album easy downloading
+// @include         https://get.google.com/albumarchive/*
+// @grant           GM_xmlhttpRequest
 // ==/UserScript==
 
-(function picasaweb_extention(){
-    var links = document.querySelectorAll('img[class^="SPRITE_"]');
-    var link_b = 'https://picasaweb.google.com/data/feed/base/user/';
-    var link_f = '?alt=rss&kind=photo&hl=en_US';
-    
-    
-    for (i=0; i<links.length; i++) {
-        var parts = /google\.com\/(\d+)\/(.+)/.exec( links[i].parentElement.getAttribute('href').split('?')[0] );
-        if( parts ){
-            links[i].parentElement.setAttribute('href', link_b + parts[1] + '/album/' + parts[2] + link_f);
-            links[i].parentElement.parentElement.parentElement.getElementsByClassName('gphoto-album-cover-title')[0].textContent = _user.nickname + ' (' + _user.name + ')\\' + parts[2];
+(function picasaweb_extention()
+{
+    var user = /\d{21}/.exec( window.location.href )[0];
+
+    GM_xmlhttpRequest({
+        method : "GET",
+        url : 'https://picasaweb.google.com/data/feed/base/user/' + user,
+        onload : function( data ){
+            var pre = 'https://picasaweb.google.com/data/feed/base/user/';
+            var post = '/?alt=rss&kind=photo&hl=en_US';
+
+            if( data ){
+                albumsList = document.createElement('div');
+                albumsList.style = 'padding: 30px 0;';
+
+                parser = new DOMParser();
+                xmlDoc = parser.parseFromString(data.responseText, "text/xml");
+                hrefs = xmlDoc.getElementsByTagName("summary");
+
+                for (i=0; i<hrefs.length; i++) {
+                    album = /\d{21}\/(.*)"><img/.exec(hrefs[i].childNodes[0].nodeValue)[1];
+                    image = /src="(.*)" alt/.    exec(hrefs[i].childNodes[0].nodeValue)[1];
+
+                    albumRss = pre + user + '/album/' + album + post;
+
+                    innerDiv = document.createElement('div');
+                    innerDiv.style = 'display: inline-block; padding: 10px;';
+
+                    albumElement       = document.createElement('a');
+                    albumElement.style = 'margin-top: -7px;padding-left: 10px;';
+                    albumElement.href  = albumRss;
+
+                    img = document.createElement('img');
+                    img.src = image;
+
+                    albumElement.appendChild(img);
+                    innerDiv.    appendChild(albumElement);
+                    albumsList.  appendChild(innerDiv);
+                }
+
+                var elem = document.querySelector('div[role=list]');
+                elem.insertBefore(albumsList, elem.childNodes[0]);
+            }
         }
-    }
+    });
 })();
